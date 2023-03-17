fabb76bea2069af731e9dd9c3624fea93ffbd249
Pedro Reyes <prodtos2@gmail.com>
2023-03-17 00:54:50 -0300

    tsc-watch

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,32 @@
-import * as express from "express";
+import express from 'express';
 
 export const createServer = ({
   port,
   path,
+  hostname,
+  queryFile,
 }: {
   port: number;
   path: string;
+  hostname?: string;
+  queryFile: (path: string) => { payload: string; contentType: string } | null;
 }) => {
   const app = express();
-  app.use("/", (req, res) => {
-    console.log("request received..");
+  app.use('/', (req, res, next) => {
+    const data = queryFile(req.path);
+    if (!data) {
+      return next();
+    }
+    res.contentType(data.contentType);
+    res.write(data.payload);
+    res.end();
+  });
+  app.use('/', (req, res) => {
+    console.log('request received..');
     res.write(`Trying to access ${req.path}`);
     res.end();
   });
-  const server = app.listen(port);
+  const server = app.listen(port, hostname || '*');
   return {
     on: server.on.bind(server),
   };
--- src/server/standalone.ts
+++ src/server/standalone.ts
@@ -1,16 +1,23 @@
 console.log(
-  "This is the standalone server runner, it is currently only used for development."
+  'This is the standalone server runner, it is currently only used for development.'
 );
 
-import { createServer } from ".";
+import { createServer } from '.';
 
 const server = createServer({
   port: 8080,
-  path: "./test_vault",
+  path: './test_vault',
+  hostname: 'localhost',
+  queryFile(path) {
+    return {
+      contentType: 'application/json',
+      payload: JSON.stringify({ env: process.env, path }),
+    };
+  },
 });
 
-server.on("error", (error) => {
-  console.log(error);
+server.on('error', (error) => {
+  console.error(error);
   process.exit(1);
 });
 
