8a4161a9ad0b773742858e025651c038c1aff2da
Pedro Reyes <prodtos2@gmail.com>
2023-03-16 21:58:35 -0300

    first prettier lint

--- scripts/start-obsidian.mjs
+++ scripts/start-obsidian.mjs
@@ -1,5 +1,5 @@
-import {spawn} from 'node:child_process'
-import {config} from 'dotenv'
+import { spawn } from "node:child_process";
+import { config } from "dotenv";
 config();
 
-spawn(process.env.OBSIDIAN_PATH)
+spawn(process.env.OBSIDIAN_PATH);
