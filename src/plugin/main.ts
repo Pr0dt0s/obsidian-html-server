fab54dbab666f740d93ad0d294e69b1bbaf95f32
Pedro Reyes <prodtos2@gmail.com>
2023-06-11 12:15:24 -0300

    move all the setup into the onLayoutReady fn

--- src/plugin/main.ts
+++ src/plugin/main.ts
@@ -14,44 +14,42 @@ export default class HtmlServerPlugin extends Plugin {
   async onload() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 
-    await new Promise<void>((resolve) =>
-      this.app.workspace.onLayoutReady(resolve)
-    );
+    this.app.workspace.onLayoutReady(async () => {
+      this.uiCleanupFns = setupUiElements(this);
 
-    this.uiCleanupFns = setupUiElements(this);
+      this.addSettingTab(new HtmlServerPluginSettingsTab(this.app, this));
 
-    this.addSettingTab(new HtmlServerPluginSettingsTab(this.app, this));
+      this.serverController = new ServerController(this);
 
-    this.serverController = new ServerController(this);
+      if (this.settings.startOnLoad) {
+        await this.startServer();
+      } else {
+        this.app.workspace.trigger('html-server-event', {
+          isServerRunning: false,
+        });
+      }
 
-    if (this.settings.startOnLoad) {
-      await this.startServer();
-    } else {
-      this.app.workspace.trigger('html-server-event', {
-        isServerRunning: false,
+      this.addCommand({
+        id: 'start-server',
+        name: 'Start the Web Server',
+        checkCallback: (checking) => {
+          if (checking) {
+            return !this.serverController?.isRunning();
+          }
+          this.startServer();
+        },
       });
-    }
 
-    this.addCommand({
-      id: 'start-server',
-      name: 'Start the Web Server',
-      checkCallback: (checking) => {
-        if (checking) {
-          return !this.serverController?.isRunning();
-        }
-        this.startServer();
-      },
-    });
-
-    this.addCommand({
-      id: 'stop-server',
-      name: 'Stop the Web Server',
-      checkCallback: (checking) => {
-        if (checking) {
-          return !!this.serverController?.isRunning();
-        }
-        this.stopServer();
-      },
+      this.addCommand({
+        id: 'stop-server',
+        name: 'Stop the Web Server',
+        checkCallback: (checking) => {
+          if (checking) {
+            return !!this.serverController?.isRunning();
+          }
+          this.stopServer();
+        },
+      });
     });
   }
 
